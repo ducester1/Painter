0e074c609dabae1a6143e798b81d9a20e5936666
Scott Schepers <schepers.scott@hotmail.com>
2017-02-17 15:48:02 +0100

    Init all files

---
+++ Painter/Painter/GameWorld.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace Painter
+{
+    class GameWorld
+    {
+        Texture2D background;
+
+        public GameWorld(ContentManager content)
+        {
+            background = content.Load<Texture2D>("spr_background");
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+            spriteBatch.Draw(background, Vector2.Zero, Color.White);
+            spriteBatch.End();
+        }
+
+    }
+}
---
+++ Painter/Painter/ThreeColorGameObject.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Painter
+{
+    class ThreeColorGameObject
+    {
+        protected Texture2D colorRed, colorGreen, colorBlue;
+        protected Texture2D currentColor;
+        protected Vector2 position, velocity;
+        protected Color color;
+
+        public ThreeColorGameObject(Texture2D colorRed, Texture2D colorGreen, Texture2D colorBlue)
+        {
+            this.colorRed = colorRed;
+            this.colorGreen = colorGreen;
+            this.colorBlue = colorBlue;
+            Color = Color.Blue;
+            position = Vector2.Zero;
+            velocity = Vector2.Zero;
+        }
+
+        public Color Color
+        {
+            get { return color; }
+            set
+            {
+                if (value != Color.Red && value != Color.Green && value != Color.Blue)
+                    return;
+                color = value;
+                if (color == Color.Red)
+                    currentColor = colorRed;
+                else if (color == Color.Green)
+                    currentColor = colorGreen;
+                else if (color == Color.Blue)
+                    currentColor = colorBlue;
+            }
+
+        }
+    }
+}
