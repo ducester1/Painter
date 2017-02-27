ae357aedf8923e96b47567a5941c0fbe81706124
Scott Schepers <schepers.scott@hotmail.com>
2017-02-27 23:48:55 +0100

    paint cans added , interaction with balls and scoring

--- Painter/Painter/GameWorld.cs
+++ Painter/Painter/GameWorld.cs
@@ -14,16 +14,28 @@ namespace Painter
         public Texture2D background;
         public Cannon cannon;
         public Ball ball;
+        public PaintCan can1;
+        public PaintCan can2;
+        public PaintCan can3;
 
         public GameWorld(ContentManager content)
         {
             background = content.Load<Texture2D>("spr_background");
             cannon = new Cannon(content);
             ball = new Ball(content);
+            can1 = new PaintCan(content, 450.0f, Color.Red);
+            can2 = new PaintCan(content, 575.0f, Color.Green);
+            can3 = new PaintCan(content, 700.0f, Color.Blue);
         }
+
         public Cannon Cannon
         {
             get { return cannon; }
+        }
+
+        public Ball Ball
+        {
+            get { return ball; }
         }
         public void HandleInput(InputHelper inputHelper)
         {
@@ -34,6 +46,9 @@ namespace Painter
         {
             ball.Reset();
             cannon.Reset();
+            can1.Reset();
+            can2.Reset();
+            can3.Reset();
         }
 
 
@@ -43,12 +58,18 @@ namespace Painter
             spriteBatch.Draw(background, Vector2.Zero, Color.White);
             ball.Draw(gameTime, spriteBatch);
             cannon.Draw(gameTime, spriteBatch);
+            can1.Draw(gameTime, spriteBatch);
+            can2.Draw(gameTime, spriteBatch);
+            can3.Draw(gameTime, spriteBatch);
             spriteBatch.End();
         }
 
         public void Update(GameTime gameTime)
         {
             ball.Update(gameTime);
+            can1.Update(gameTime);
+            can2.Update(gameTime);
+            can3.Update(gameTime);
         }
         public bool IsOutsideWorld(Vector2 position)
         {
